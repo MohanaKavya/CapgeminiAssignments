963530e2d73cfc527f2501081cd7602ff088d990
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-24 16:53:55 +0530

    Added: Part Time Employee Attendance and Daily Wage Calculation is Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
 	
 	        //constants
 			public static final int IS_FULL_TIME = 1;
+			public static final int IS_PART_TIME = 2;
 			public static final int EMP_WAGE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
@@ -16,13 +17,18 @@ public class EmployeeWage {
 			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 			
 			// Computation
-			double empCheck = Math.floor(Math.random()*10)%2;
+			double empCheck = Math.floor(Math.random()*10)%3;
 			
 			// if-else to Check and Display Employee is Present or Absent 
 			if(empCheck == IS_FULL_TIME) {
 				System.out.println("Employee Present");
 				empHours = 8;
 			}	
+			else 
+				if(empCheck == IS_PART_TIME){
+				System.out.println("Employee Present - Part Time");
+				empHours = 4;	
+			}
 			else 
 				System.out.println("Employee Absent");	
 			
