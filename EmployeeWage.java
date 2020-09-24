07eb80186c7a0b0441f3008bb1571e335109122d
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-24 16:59:30 +0530

    Modified: if-else Statements Modified to Switch Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,7 @@ package AssignmentOne;
 
 public class EmployeeWage {
 	
-	        //constants
+	        // constants
 			public static final int IS_FULL_TIME = 1;
 			public static final int IS_PART_TIME = 2;
 			public static final int EMP_WAGE_PER_HOUR = 20;
@@ -17,20 +17,19 @@ public class EmployeeWage {
 			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 			
 			// Computation
-			double empCheck = Math.floor(Math.random()*10)%3;
+			int empCheck = (int)(Math.floor(Math.random()*10)%3);
 			
-			// if-else to Check and Display Employee is Present or Absent 
-			if(empCheck == IS_FULL_TIME) {
-				System.out.println("Employee Present");
-				empHours = 8;
-			}	
-			else 
-				if(empCheck == IS_PART_TIME){
-				System.out.println("Employee Present - Part Time");
-				empHours = 4;	
+			// if-else changed to Switch to Check and Display Employee is Present or Absent 
+			switch(empCheck)
+			{ case IS_FULL_TIME : System.out.println("Employee Present - Full Time");
+								  empHours = 8;
+								  break;
+			  case IS_PART_TIME : System.out.println("Employee Present - Part Time");
+			  					  empHours = 4;
+			  					  break;
+			  default           : System.out.println("Employee Absent");
+			
 			}
-			else 
-				System.out.println("Employee Absent");	
 			
 			// Calculation of Daily Employee Wage
 			empWage = empHours * EMP_WAGE_PER_HOUR;
