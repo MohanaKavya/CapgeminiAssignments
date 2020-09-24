c648e336ecc04883dd6560fa9a361f3b7662e252
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-24 14:00:27 +0530

    Added: Welcome Message Added

---
+++ EmployeeWage.java
@@ -0,0 +1,10 @@
+package AssignmentOne;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+			// Welcome Message		
+			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+}
