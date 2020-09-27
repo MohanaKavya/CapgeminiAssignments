17774a910ee6550543e87e3b75275965e8483aaa
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-09-27 23:44:04 +0530

    Added: Employee Wage Calculation for Different Companies is Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,73 +2,44 @@ package AssignmentOne;
 
 public class EmployeeWage {
 	
-	        // constants
-			public static final int IS_FULL_TIME = 1;
-			public static final int IS_PART_TIME = 2;
-			public static final int EMP_WAGE_PER_HOUR = 20;
-			public static final int WORKDAYS_PER_MONTH = 20;
-			public static final int WORKHOURS_PER_MONTH = 100;
 			
-			// Refactor to Compute Employee Wage Method in Class
-			public int computeEmployeeWage(int empHours) {
-			// variables
-			int empWage = 0;
-			int dayCount = 0;
-			int hoursWorked = 0;
-			int totalWage = 0;
-			
-			// Calculation of Daily Employee Wage
-			empWage = empHours * EMP_WAGE_PER_HOUR;
-						
-			// Tabular Display of Employee Details for Maximum Monthly Hours or Days
-			System.out.println("Day\tHours Worked\tTotal Wage");
-			while((hoursWorked+empHours)<=WORKHOURS_PER_MONTH && dayCount<WORKDAYS_PER_MONTH)
-				{	
-						dayCount++;
-						hoursWorked += empHours;
-						totalWage += empWage;
-						System.out.println(" "+dayCount+"  \t"+hoursWorked+"  \t\t"+totalWage);
-							
-				}
-						
-			// Calculations of Employee Wage for Hours left out of Maximum Hours
-			if(hoursWorked<WORKHOURS_PER_MONTH && dayCount<WORKDAYS_PER_MONTH)
-			{
-						dayCount++;
-						totalWage += (WORKHOURS_PER_MONTH - hoursWorked) * EMP_WAGE_PER_HOUR;
-						hoursWorked = hoursWorked + (WORKHOURS_PER_MONTH - hoursWorked);
-						System.out.println(" "+dayCount+"  \t"+hoursWorked+"  \t\t"+totalWage);
-			}	
-			return totalWage;
-			}
-
 	public static void main(String[] args) {
 		
 			// variables 
-			int empHours = 0;
+			int emp_wage_per_hour = 0;
+			int workdays_per_month = 0;
+			int workhours_per_month = 0; 
 			
 			// Welcome Message		
 			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 			
-			// Computation
-			int empCheck = (int)(Math.floor(Math.random()*10)%3);
+			// Modified : Computation to Select Company
+			int company_check = (int)(Math.floor(Math.random()*10)%3);
 			
-			// if-else changed to Switch to Check and Display Employee is Present or Absent 
-			switch(empCheck)
-			{ case IS_FULL_TIME : System.out.println("Employee Present - Full Time");
-								  empHours = 8;
+			// Switch Case to Select a Company 
+			switch(company_check)
+			{ 			case 0 :  System.out.println("Company A");
+								  emp_wage_per_hour = 20;
+								  workdays_per_month = 20;
+								  workhours_per_month = 100;
 								  break;
-			  case IS_PART_TIME : System.out.println("Employee Present - Part Time");
-			  					  empHours = 4;
+			            case 1 :  System.out.println("Company B");
+						  		  emp_wage_per_hour = 10;
+						  		  workdays_per_month = 15;
+						  		  workhours_per_month = 80;
 			  					  break;
-			  default           : System.out.println("Employee Absent");
-			
+			            case 2 :  System.out.println("Company C");
+			            		  emp_wage_per_hour = 30;
+			            		  workdays_per_month = 25;
+			            		  workhours_per_month = 140;	
 			}
 			
-			EmployeeWage employee = new EmployeeWage();
+			// Instantiation of Company Class
+			Company company = new Company(emp_wage_per_hour, workdays_per_month, workhours_per_month);
+			company.employeeAttendance();
 			
-			int totalWage = employee.computeEmployeeWage(empHours);
-			
+			int totalWage = company.computeEmployeeWage();
+		
 			// Display of Employee Total Wage
 			System.out.println("The Employee Total Wage for Maximum Hours or Days : "+totalWage);
 			
